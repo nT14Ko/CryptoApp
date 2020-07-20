590f1f68868d42560342da7bfdd3316ee1d1f09f
Nikorych Mykola <nt14koo@gmail.com>
2020-07-20 21:07:38 +0300

    Added ApiFactory and ApiService, RxJava2 implementations, updated minSdkVersion to 21

--- app/src/main/java/com/nikorych/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/nikorych/cryptoapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.nikorych.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.nikorych.cryptoapp.api.ApiFactory
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.schedulers.Schedulers.io
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val disposable = ApiFactory.apiService.getFullPriceList(fSyms = "BTC, ETH, ")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            },{
+                Log.d("TEST_OF_LOADING_DATA", it.message)
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
